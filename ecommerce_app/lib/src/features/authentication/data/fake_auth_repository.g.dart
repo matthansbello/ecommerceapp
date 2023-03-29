0feb5278df86c6108effad70b16d84b53f923bf0
Matthew Hans-Bello <matthewhansbello@live.com>
2023-03-29 15:13:23 +0100

    first commit

---
+++ ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fake_auth_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authRepositoryHash() => r'f94389a2b4b189c4aeaf4b993818ca745215d122';
+
+/// See also [authRepository].
+@ProviderFor(authRepository)
+final authRepositoryProvider = Provider<FakeAuthRepository>.internal(
+  authRepository,
+  name: r'authRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AuthRepositoryRef = ProviderRef<FakeAuthRepository>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
---
+++ ecommerce_app/lib/src/features/reviews/data/fake_reviews_repository.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fake_reviews_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$reviewsRepositoryHash() => r'8a69281fa37026cd50057faf754621a4e46629e5';
+
+/// See also [reviewsRepository].
+@ProviderFor(reviewsRepository)
+final reviewsRepositoryProvider = Provider<FakeReviewsRepository>.internal(
+  reviewsRepository,
+  name: r'reviewsRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$reviewsRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ReviewsRepositoryRef = ProviderRef<FakeReviewsRepository>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
